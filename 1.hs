e2753bbdb9ca157efedcf8cf33266f0e0a4b2fb1
Bree Elle Gardner <letsbreelhere@gmail.com>
2019-12-01 23:00:00 -0800

    Nicer haskell

--- 1.hs
+++ 1.hs
@@ -8,17 +8,11 @@ sumBy f = getSum . foldMap (Sum . f)
 fuelRequired :: Int -> Int
 fuelRequired = subtract 2 . (`div` 3)
 
-part1 :: [Int] -> Int
-part1 = sumBy fuelRequired
-
 repeatingFuelRequired :: Int -> Int
 repeatingFuelRequired = sum . tail . takeWhile (>0) . iterate fuelRequired
-
-part2 :: [Int] -> Int
-part2 = sumBy repeatingFuelRequired
 
 main :: IO ()
 main = do
   values <- map read . lines <$> readFile "1.txt" :: IO [Int]
-  print $ part1 values
-  print $ part2 values
+  print $ sumBy fuelRequired values
+  print $ sumBy repeatingFuelRequired values
