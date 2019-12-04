dbe361486c704d7fb7a01e8420387339cc08f7a6
Bree Elle Gardner <letsbreelhere@gmail.com>
2019-12-04 10:36:09 -0800

    Day 4

---
+++ 4.hs
@@ -0,0 +1,40 @@
+module DayFour where
+
+monotonic' :: Ord a => a -> [a] -> Bool
+monotonic' y (x:xs) = y <= x && monotonic' x xs
+monotonic' _ [] = True
+
+monotonic :: Ord a => [a] -> Bool
+monotonic [] = True
+monotonic (x:xs) = monotonic' x xs
+
+adjacentEqual :: Eq a => [a] -> Bool
+adjacentEqual (x:x':xs) =
+  x == x' || adjacentEqual (x':xs)
+adjacentEqual _ = False
+
+isValidPassword :: Int -> Bool
+isValidPassword x =
+  let ds = show x
+   in monotonic ds && adjacentEqual ds
+
+containsDouble :: Eq a => [a] -> Bool
+containsDouble ys@(x:x':xs) =
+  let (ls, rs) = span (== x) ys
+   in length ls == 2 || containsDouble rs
+containsDouble _ = False
+
+isValidPassword' :: Int -> Bool
+isValidPassword' x =
+  let ds = show x
+   in monotonic ds && containsDouble ds
+
+input :: [Int]
+input = [145852..616942]
+
+main :: IO ()
+main = do
+  -- Part 1
+  print . length . filter isValidPassword $ input
+  -- Part 2
+  print . length . filter isValidPassword' $ input
