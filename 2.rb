c45d94edc974834235317acd493febaad1cde683
Bree Elle Gardner <letsbreelhere@gmail.com>
2019-12-06 13:46:08 -0800

    Separating intcode emulator

--- 2.rb
+++ 2.rb
@@ -1,46 +1,6 @@
 # frozen_string_literal: true
 
-class Intcode
-  attr_reader :ints, :ip, :halt
-
-  UnknownOpcode = Struct.new(:opcode)
-
-  def initialize(ints)
-    @ints = ints.dup
-    @ip = 0
-    @halt = false
-  end
-
-  def opcode
-    @ints[@ip]
-  end
-
-  def step
-    if ints.length - ip > 2
-      operand1 = ints[ints[ip + 1]]
-      operand2 = ints[ints[ip + 2]]
-      result_ip = ints[ip + 3]
-    end
-
-    case opcode
-    when 1
-      @ints[result_ip] = operand1 + operand2
-      @ip += 4
-    when 2
-      @ints[result_ip] = operand1 * operand2
-      @ip += 4
-    when 99
-      @halt = true
-    else
-      raise UnknownOpcode, opcode
-    end
-  end
-
-  def output
-    step until halt
-    ints[0]
-  end
-end
+require_relative 'intcode'
 
 def output_with_noun_and_verb(ints, noun, verb)
   new_ints = ints.dup
