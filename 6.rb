bcff4302d0eef0bd2eaed785afe309d103b78bff
Bree Elle Gardner <letsbreelhere@gmail.com>
2019-12-06 13:29:13 -0800

    Restructuring

--- 6.rb
+++ 6.rb
@@ -1,63 +1,67 @@
 # frozen_string_literal: true
 
 require 'pry'
+require 'set'
 
-def build_tree(edges)
-  tree = {}
-  edges.each do |parent, child|
-    tree[parent] ||= []
-    tree[parent] << child
+class Tree
+  attr_reader :edges
+
+  def initialize(edges)
+    @edges = edges
+    @parents = {}
   end
 
-  tree
-end
+  def self.from_edges(edges)
+    adj = {}
+    edges.each do |parent, child|
+      adj[parent] ||= []
+      adj[parent] << child
+    end
 
-def parents(tree, node)
-  parent = tree.find { |_, c| c.include?(node) }
-  if parent
-    [node] + parents(tree, parent[0])
-  else
-    []
+    new(adj)
+  end
+
+  def height(node)
+    parents(node).length
+  end
+
+  def nodes
+    (edges.values.flatten + edges.keys).uniq
+  end
+
+  def distance_between(l, r)
+    lub = least_upper_bound(l, r)
+    height_to(lub, l) + height_to(lub, r)
+  end
+
+  private
+
+  def least_upper_bound(l, r)
+    r_parents = Set.new(parents(r))
+    parents(l).find { |p| r_parents.include?(p) }
+  end
+
+  def height_to(parent, child)
+    parents(child).index(parent)
+  end
+
+  def parents(node)
+    return @parents[node] if @parents[node]
+
+    parent = edges.find { |_, c| c.include?(node) }
+    @parents[node] = if parent
+                       [node] + parents(parent.first)
+                     else
+                       []
+                     end
   end
 end
 
-@heights = {}
-def height(tree, node)
-  return @heights[node] if @heights[node]
-
-  parent = tree.find { |_, c| c.include?(node) }
-  @heights[node] = if parent
-                     1 + height(tree, parent[0])
-                   else
-                     0
-  end
-end
-
-def height_to(tree, parent, child)
-  return 0 if child == parent
-
-  p = tree.find { |_, c| c.include?(child) }
-
-  if p
-    1 + height_to(tree, parent, p[0])
-  else
-    0
- end
-end
-
-def distance_between(tree, l, r)
-  l_parents = parents(tree, l)
-  r_parents = parents(tree, r)
-  lub = (l_parents & r_parents)[0]
-  l_h = height_to(tree, lub, l)
-  r_h = height_to(tree, lub, r)
-  l_h + r_h - 2
-end
+tree = Tree.from_edges(
+  File.read('6.txt').split("\n").map { |line| line.split(')') }
+)
 
 # Part 1
-tree = build_tree(File.read('6.txt').split("\n").map { |line| line.split(')') })
-nodes = (tree.values.flatten + tree.keys).uniq
-p nodes.map { |p| height(tree, p) }.sum
-
+puts tree.nodes.map { |p| tree.height(p) }.sum
 # Part 2
-puts distance_between(tree, 'YOU', 'SAN')
+puts tree.distance_between('YOU', 'SAN') - 2
