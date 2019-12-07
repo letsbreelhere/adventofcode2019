0eb636a938c65cf77b21579357a13fdb49d585e5
Bree Elle Gardner <letsbreelhere@gmail.com>
2019-12-06 17:16:22 -0800

    Ruthless, idiotic code golf

--- 6.hs
+++ 6.hs
@@ -1,61 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Safe
+import Data.List (intersect)
 import Data.Map (Map)
-import Data.Maybe (maybe)
-import Data.List
-import Data.Tuple
 import qualified Data.Map as M
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Safe (headMay)
 
-type Tree = Map String String
+type Tree = Map Text Text
 
-split' :: Eq a => [a] -> a -> [a] -> ([a], [a])
-split' ls c [] = (ls, [])
-split' ls c (x:xs)
-  | c == x = (ls, xs)
-  | otherwise = split' (ls ++ [x]) c xs
+parseTree :: Text -> Tree
+parseTree = M.fromList . map ((\[p, c] -> (c, p)) . T.splitOn ")") . T.lines
 
-split = split' []
-
-edges :: String -> [(String, String)]
-edges = map (split ')') . lines
-
-parseTree :: String -> Tree
-parseTree = M.fromList . map swap . edges
-
-parentsOf :: Tree -> String -> [String]
+parentsOf :: Tree -> Text -> [Text]
 parentsOf t s =
   case M.lookup s t of
     Nothing -> []
     Just p -> p : parentsOf t p
 
-heightOf :: Tree -> String -> Int
-heightOf t = length . parentsOf t
-
-heightTo :: Tree -> String -> String -> Maybe Int
+heightTo :: Tree -> Text -> Text -> Maybe Int
 heightTo t root child
   | root == child = Just 0
-  | otherwise = do
-    p <- M.lookup child t
-    (1 +) <$> heightTo t root p
+  | otherwise = fmap (1 +) (heightTo t root =<< M.lookup child t)
 
-leastUpperBound :: Tree -> String -> String -> Maybe String
-leastUpperBound t l r =
-  let lParents = parentsOf t l
-      rParents = parentsOf t r
-   in headMay (lParents `intersect` rParents)
+leastUpperBound :: Tree -> Text -> Text -> Maybe Text
+leastUpperBound t l r = headMay (parentsOf t l `intersect` parentsOf t r)
 
-distanceBetween :: Tree -> String -> String -> Maybe Int
+distanceBetween :: Tree -> Text -> Text -> Maybe Int
 distanceBetween t l r = do
   lub <- leastUpperBound t l r
-  lh <- heightTo t lub l
-  rh <- heightTo t lub r
-  pure (lh + rh)
+  (+) <$> heightTo t lub l <*> heightTo t lub r
 
 main :: IO ()
 main = do
-  t <- parseTree <$> readFile "6.txt"
+  t <- parseTree <$> T.readFile "6.txt"
   -- Part 1
-  print . sum . map (heightOf t) $ M.keys t
+  print . sum . map (length . parentsOf t) $ M.keys t
   -- Part 2
   print . fmap (subtract 2) $ distanceBetween t "YOU" "SAN"
