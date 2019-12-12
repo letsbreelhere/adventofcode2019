51b8ac8879fef0d9b8994cadf0e66ff7ad97dfc2
Bree Elle Gardner <letsbreelhere@gmail.com>
2019-12-12 00:53:14 -0800

    🧹

--- haskell-solutions/app/12.hs
+++ haskell-solutions/app/12.hs
@@ -1,42 +1,50 @@
 module Main where
 
 import Control.Lens
-import Data.Maybe
 import Data.Foldable
 import Data.List
-import Linear.V3
+import Data.Maybe
 import Data.Set (Set)
-import Debug.Trace
 import qualified Data.Set as S
+import Linear.V3
 
 type Point = V3 Int
+
 data Moon = Moon
   { _pos :: Point
   , _vel :: Point
   } deriving (Show)
+
 makeLenses ''Moon
+
+type Universe = [Moon]
 
 mkMoon :: Point -> Moon
 mkMoon p = Moon p (V3 0 0 0)
 
-velocityDeltaAt :: Int -> [Moon] -> Point
+velocityDeltaAt :: Int -> Universe -> Point
 velocityDeltaAt i ms =
   let m = ms !! i
-      ms' = take i ms ++ drop (i+1) ms
-      deltas = map (\m' -> signum (m'^.pos - m^.pos)) ms'
-   in sum deltas
+      ms' = take i ms ++ drop (i + 1) ms
+      deltas = map (\m' -> signum (m' ^. pos - m ^. pos)) ms'
+  in sum deltas
 
-step :: [Moon] -> [Moon]
+step :: Universe -> Universe
 step ms =
-  let ds = map (`velocityDeltaAt` ms) [0..length ms]
+  let ds = map (`velocityDeltaAt` ms) [0 .. length ms]
       ms' = zipWith (\m d -> m & vel +~ d) ms ds
-   in map (\m -> let v = m^.vel in m & pos +~ v) ms'
+  in map
+       (\m ->
+          let v = m ^. vel
+          in m & pos +~ v)
+       ms'
 
 energy :: Moon -> Int
 energy (Moon p v) = mag p * mag v
-  where mag = sum . toList . abs
+  where
+    mag = sum . toList . abs
 
-input :: [Moon]
+input :: Universe
 input =
   [ mkMoon (V3 3 (-6) 6)
   , mkMoon (V3 10 7 (-9))
@@ -44,49 +52,28 @@ input =
   , mkMoon (V3 (-8) 0 4)
   ]
 
-atStep :: [Moon] -> Int -> [Moon]
+atStep :: Universe -> Int -> Universe
 atStep ms s = iterate step ms !! s
 
 -- This should be a stream but I'm feeling lazy.
-periodOf :: Ord a => [a] -> (a, Int)
+periodOf :: Ord a => [a] -> Int
 periodOf = go S.empty 0
-  where go m n (x:xs) = (if n `mod` 10000 == 0 then traceShow n else id) $
-          if S.member x m
-            then (x, n)
-            else go (S.insert x m) (n+1) xs
+  where
+    go m n (x:xs) =
+      if S.member x m
+        then n
+        else go (S.insert x m) (n + 1) xs
 
-test :: [Moon]
-test =
-  [ mkMoon (V3 (-1) 0 2)
-  , mkMoon (V3 2 (-10) (-7))
-  , mkMoon (V3 4 (-8) 8)
-  , mkMoon (V3 3 5 (-1))
-  ]
+stepAxes :: Getting Int (V3 Int) Int -> [[(Int, Int)]]
+stepAxes l = map (map (get l)) (iterate step input)
+  where get l m = (m ^. vel . l, m ^. pos . l)
 
 main :: IO ()
 main = do
   let stepped = input `atStep` 1000
       part1 = sum . map energy $ stepped
   print part1
-  let ss = iterate step input
-      getX m = (m^.vel._x, m^.pos._x)
-      getY m = (m^.vel._y, m^.pos._y)
-      getZ m = (m^.vel._z, m^.pos._z)
-
-      xs = map (map getX) ss
-      (xp, px) = periodOf xs
-  print (xp, px)
-
-  let ys = map (map getY) ss
-      (yp, py) = periodOf ys
-  print (yp, py)
-
-  let zs = map (map getZ) ss
-      (zp, pz) = periodOf zs
-  print (zp, pz)
-
-  let Just firstX = elemIndex xp xs
-      Just firstY = elemIndex yp ys
-      Just firstZ = elemIndex zp zs
-      l = foldr lcm 1 [px, py, pz]
-  print l
+  let px = periodOf (stepAxes _x)
+      py = periodOf (stepAxes _y)
+      pz = periodOf (stepAxes _z)
+  print (foldr lcm 1 [px, py, pz])
