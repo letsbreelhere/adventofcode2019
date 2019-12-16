110d33289d0eae34f9708159501dbf38384a2292
Bree Elle Gardner <letsbreelhere@gmail.com>
2019-12-15 16:29:22 -0800

    Almost-working mess

--- haskell-solutions/app/14.hs
+++ haskell-solutions/app/14.hs
@@ -6,10 +6,12 @@ import Control.Monad.State
 import Data.Attoparsec.Text hiding (count)
 import Data.Map (Map)
 import qualified Data.Map as M
+import Data.Map.Merge.Strict
 import Data.Maybe
 import qualified Data.Text as T
 import Data.Text (Text)
 import qualified Data.Text.IO as T
+import Debug.Trace
 
 data Factor = Factor
   { _count :: Int
@@ -45,67 +47,76 @@ parseDigraph :: Parser (Map Text Formula)
 parseDigraph = fmap M.fromList (parseFormula `sepBy1` endOfLine)
 
 data WalkState = WalkState
-  { _target :: Factor
-  , _existing :: Map Text Int
+  { _existing :: Map Text Int
   , _digraph :: Digraph
-  }
-  deriving (Show)
+  } deriving (Show)
 
 defaultWalkState :: Digraph -> WalkState
-defaultWalkState d =
-  WalkState
-  { _target = Factor 1 "FUEL"
-  , _existing = M.empty
-  , _digraph = d
-  }
+defaultWalkState d = WalkState {_existing = M.empty, _digraph = d}
+
 makeLenses ''WalkState
 
 type Walk a = State WalkState a
 
-needed :: Walk (Int, Map Text Int)
-needed = do
-  Factor multiplier t <- use target
+divRoundUp :: Int -> Int -> Int
+divRoundUp a b
+  | r == 0 = q
+  | otherwise = q + 1
+  where
+    (q, r) = a `divMod` b
+
+neededFromFormula :: Int -> Formula -> [(Text, Int)]
+neededFromFormula repeatCount f =
+  let ins = f ^. inputs
+      outputMultiple = f ^. outputCount
+      requiredForFactor (Factor inputMultiple l) =
+        (l, repeatCount * inputMultiple)
+  in map requiredForFactor ins
+
+needed :: Text -> Int -> Walk (Int, Map Text Int)
+needed t desiredCount = do
   d <- use digraph
   case M.lookup t d of
     Nothing -> pure (0, M.empty)
     Just formula -> do
-      let factors = M.fromList . map ((view label &&& view count) . (count *~ multiplier)) . view inputs $ formula
-      pure (formula^.outputCount, factors)
+      let repeatCount = desiredCount `divRoundUp` outputMultiple
+          factors = M.fromList . neededFromFormula repeatCount $ formula
+          outputMultiple = formula ^. outputCount
+      pure (repeatCount * outputMultiple, factors)
+
+difference :: (Ord a, Ord n, Num n) => Map a n -> Map a n -> Map a n
+difference =
+  merge
+    preserveMissing
+    dropMissing
+    (zipWithMatched $ \_ l r -> max (l - r) 0)
 
 removeExistingFrom :: Map Text Int -> Walk (Map Text Int)
-removeExistingFrom = undefined
+removeExistingFrom m = do
+  e <- use existing
+  existing .= e `difference` m
+  pure (m `difference` e)
 
 -- If the current target is ore, we're done; return the amount of the target.
 -- Otherwise:
---  1. If we have any leftover elements that apply to the current requirements,
---     they annihilate one another.
+--  1. Remove any leftover products from the requirements list
 --  2. Walk the the nodes under the current target and recur.
 --  3. If we produced more than we needed, add the remainder to _existing.
-minOre :: Walk Int
-minOre = do
-  t <- use target
-  if t^.label == "ORE"
-     then pure (t^.count)
-     else do
-       (produced, factors) <- needed
-       requirements <- removeExistingFrom factors
-       result <- fmap sum . mapM minOreAt . M.toList $ requirements
-       let leftover = produced - t^.count
-       if leftover < 0
-          then fail "Didn't produce enough products"
-          else pure ()
-       existing %= M.insertWith (+) (t^.label) leftover
-       pure result
-
-minOreAt :: (Text, Int) -> Walk Int
-minOreAt (l, c) = do
-  target .= Factor c l
-  minOre
+minOre :: Text -> Int -> Walk Int
+minOre "ORE" targetCount = pure targetCount
+minOre targetLabel targetCount = do
+  (produced, factors) <- needed targetLabel targetCount
+  requirements <- removeExistingFrom factors
+  e <- use existing
+  result <- fmap sum . mapM (uncurry minOre) . M.toList $ requirements
+  let leftover = produced - targetCount
+  existing %= M.insertWith (+) targetLabel leftover
+  pure result
 
 walk :: Walk a -> Digraph -> a
 walk w d = evalState w (defaultWalkState d)
 
 main :: IO ()
 main = do
-  Right digraph <- parseOnly parseDigraph <$> T.readFile "sample.txt"
-  print . walk minOre $ digraph
+  Right digraph <- parseOnly parseDigraph <$> T.readFile "./sample.txt"
+  print . walk (minOre "FUEL" 1) $ digraph
