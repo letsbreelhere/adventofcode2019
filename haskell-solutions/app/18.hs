e9a13017ffde16da810b887c2dbd734c15c2c58e
Bree Elle Gardner <letsbreelhere@gmail.com>
2019-12-20 23:24:38 -0500

    Tinkering on some helpers

--- haskell-solutions/app/18.hs
+++ haskell-solutions/app/18.hs
@@ -2,6 +2,7 @@
 
 module Main where
 
+import Data.Bifunctor
 import qualified Data.Sequence as Seq
 import qualified Data.Set as S
 import Data.List
@@ -22,6 +23,7 @@ data Tile
   deriving (Show, Eq, Ord)
 
 type Layout = Map Point Tile
+type MazeGraph = Graph Dir (Point, Tile)
 
 parseLayout :: String -> Layout
 parseLayout = snd . foldr parseRow (0, M.empty) . lines
@@ -40,15 +42,53 @@ tileFromChar c = case c of
                    '.' -> Just Floor
                    '@' -> Just Me
                    c | isLetter c ->
-                     Just (if isUpper c then Door c else Key c)
+                     Just (if isUpper c then Door (toLower c) else Key c)
                    '#' -> Nothing
                    _ -> error ("Unexpected tile " ++ show c)
 
+useKey :: Char -> MazeGraph -> MazeGraph
+useKey c = mapVertices (second (openDoor . removeKey))
+  where openDoor (Door c') | c' == c = Floor
+        openDoor t = t
+        removeKey (Key c') | c' == c = Floor
+        removeKey t = t
+
+removeDoors :: MazeGraph -> MazeGraph
+removeDoors = filterVertices (not . isDoor)
+  where isDoor (_, Door _) = True
+        isDoor _ = False
+
+withoutKeyChildren :: Tree Dir (Point, Tile) -> Tree Dir (Point, Tile)
+withoutKeyChildren (Tree v@(_,t) cs) =
+  case t of
+    Key _ -> Tree v M.empty
+    _ -> Tree v (M.map withoutKeyChildren cs)
+
+filterTree :: (v -> Bool) -> Tree e v -> [v]
+filterTree p (Tree v cs) =
+  let children = concatMap (filterTree p) (map snd (M.toList cs))
+   in if p v
+         then v : children
+         else children
+
+availableKeys' :: Tree Dir (Point, Tile) -> [(Point, Char)]
+availableKeys' (Tree v cs) =
+  let rest = concatMap availableKeys' (map snd (M.toList cs))
+   in case v of
+        (p, Key c) -> (p, c) : rest
+        _ -> rest
+
+availableKeys :: Point -> MazeGraph -> [(Point, Char)]
+availableKeys p g = availableKeys' (bfs (p, Floor) g)
+
 main :: IO ()
 main = do
-  input <- readFile "sample.txt"
+  input <- readFile "./sample.txt"
   let layout = parseLayout input
-      Just curPos = fmap fst . find ((Me ==) . snd) . M.toList $ layout
-      g = toDirGraph layout
-      t = bfs (curPos, Me) g
-  print t
+      Just (curPos, _) = find ((Me ==) . snd) . M.toList $ layout
+      meToFloor t = case t of
+                      Me -> Floor
+                      _ -> t
+      g = mapVertices (second meToFloor) (toDirGraph layout)
+      t = bfs (curPos, Floor) $ g
+  print (withoutKeyChildren t)
--- haskell-solutions/src/Graph.hs
+++ haskell-solutions/src/Graph.hs
@@ -12,39 +12,43 @@ import Data.Maybe
 import Control.Lens hiding ((:<))
 import Dir
 
-data Graph v e = Graph { getAdj :: Map v (Map e v) }
+data Graph e v = Graph { getAdj :: Map v (Map e v) }
   deriving (Show, Eq)
 
-neighbors :: Ord v => v -> Graph v e -> Map e v
+-- Not a functor instance, because it doesn't satisfy the laws.
+mapVertices :: (Ord v, Ord v') => (v -> v') -> Graph e v -> Graph e v'
+mapVertices f = Graph . M.mapKeys f . M.map (M.map f) . getAdj
+
+neighbors :: Ord v => v -> Graph e v -> Map e v
 neighbors v = fromMaybe M.empty . M.lookup v . getAdj
 
-addEdge :: (Ord v, Ord e) => v -> e -> v -> Graph v e -> Graph v e
+addEdge :: (Ord v, Ord e) => v -> e -> v -> Graph e v -> Graph e v
 addEdge v e v' (Graph g) = Graph $ M.insertWith M.union v (M.singleton e v') g
 
 emptyGraph = Graph M.empty
 
-data Tree v e =
+data Tree e v =
   Tree v
-       (Map e (Tree v e))
+       (Map e (Tree e v))
 
-showTree :: (Show v, Show e) => Int -> Tree v e -> [String]
+showTree :: (Show v, Show e) => Int -> Tree e v -> [String]
 showTree indent (Tree v cs) =
   let children = M.toList cs
       tabs = concat $ replicate indent "  "
    in (tabs ++ show v) : concatMap (\(e, t') -> (tabs ++ show e ++ ":") : showTree (indent+1) t') children
 
-instance (Show v, Show e) => Show (Tree v e) where
+instance (Show v, Show e) => Show (Tree e v) where
   show = unlines . showTree 0
 
-toTree' :: (Ord e, Ord v) => Set v -> v -> Graph v e -> Maybe (Tree v e)
+toTree' :: (Ord e, Ord v) => Set v -> v -> Graph e v -> Maybe (Tree e v)
 toTree' s v g
   | S.member v s = Nothing
   | otherwise = Just $ Tree v (M.fromList . mapMaybe (\(e,v') -> (e,) <$> toTree' (S.insert v s) v' g) . M.toList $ neighbors v g)
 
-toTree :: (Ord e, Ord v) => v -> Graph v e -> Tree v e
+toTree :: (Ord e, Ord v) => v -> Graph e v -> Tree e v
 toTree v g = fromJust $ toTree' S.empty v g
 
-toDirGraph :: Ord v => Map Point v -> Graph (Point, v) Dir
+toDirGraph :: Ord v => Map Point v -> Graph Dir (Point, v)
 toDirGraph m = Graph . M.fromList . map (\(p, v) -> ((p, v), neighbors p)) . M.toList $ m
   where neighbors p = M.fromList $ mapMaybe (neighborAt p) [N, E, W, S]
         neighborAt p dir = do
@@ -52,12 +56,22 @@ toDirGraph m = Graph . M.fromList . map (\(p, v) -> ((p, v), neighbors p)) . M.t
           v' <- M.lookup p' m
           pure (dir, (p', v'))
 
-addEdgeUnlessVisited :: (Ord v, Ord e) => Set v -> v -> e -> v -> Graph v e -> Graph v e
+addEdgeUnlessVisited :: (Ord v, Ord e) => Set v -> v -> e -> v -> Graph e v -> Graph e v
 addEdgeUnlessVisited visited v e v' (Graph g)
   | S.member v' visited = Graph g
   | otherwise = Graph $ M.insertWith M.union v (M.singleton e v') g
 
-dfs' :: (Ord v, Ord e) => [(Maybe (v, e), v)] -> Set v -> Graph v e -> Graph v e -> Graph v e
+deleteVertex :: (Ord v, Ord e) => v -> Graph e v -> Graph e v
+deleteVertex v = filterVertices (/= v)
+
+
+filterVertices :: (Ord v, Ord e) => (v -> Bool) -> Graph e v -> Graph e v
+filterVertices p (Graph g) =
+  let g' = M.filterWithKey (\v _ -> not (p v)) g
+      removeChildren = M.filter (not . p)
+   in Graph (M.map removeChildren g')
+
+dfs' :: (Ord v, Ord e) => [(Maybe (v, e), v)] -> Set v -> Graph e v -> Graph e v -> Graph e v
 dfs' [] _ out _ = out
 dfs' ((parentData, v) : toVisit) visited out g
   | S.member v visited = dfs' toVisit visited out g
@@ -70,10 +84,10 @@ dfs' ((parentData, v) : toVisit) visited out g
         toVisit' = (map (\(e, v') -> (Just (v, e), v')) curNeighbors) ++ toVisit
     in dfs' toVisit' visited' out' g
 
-dfs :: (Ord v, Ord e) => v -> Graph v e -> Tree v e
+dfs :: (Ord v, Ord e) => v -> Graph e v -> Tree e v
 dfs v = toTree v . dfs' [(Nothing, v)] S.empty emptyGraph
 
-bfs' :: (Ord v, Ord e) => Seq (Maybe (v, e), v) -> Set v -> Graph v e -> Graph v e -> Graph v e
+bfs' :: (Ord v, Ord e) => Seq (Maybe (v, e), v) -> Set v -> Graph e v -> Graph e v -> Graph e v
 bfs' (viewl -> EmptyL) _ out _ = out
 bfs' (viewl -> (parentData, v) :< toVisit) visited out g
   | S.member v visited = bfs' toVisit visited out g
@@ -86,10 +100,10 @@ bfs' (viewl -> (parentData, v) :< toVisit) visited out g
         toVisit' = toVisit <> Seq.fromList (map (\(e, v') -> (Just (v, e), v')) curNeighbors)
     in bfs' toVisit' visited' out' g
 
-bfs :: (Ord v, Ord e) => v -> Graph v e -> Tree v e
+bfs :: (Ord v, Ord e) => v -> Graph e v -> Tree e v
 bfs v = toTree v . bfs' (Seq.singleton (Nothing, v)) S.empty emptyGraph
 
-height :: Tree v e -> Int
+height :: Tree e v -> Int
 height (Tree _ cs) =
   let heights = map (\(_, t') -> 1 + height t') (M.toList cs)
    in maximum (0 : heights)
